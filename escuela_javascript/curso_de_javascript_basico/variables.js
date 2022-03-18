66c912e35b74411d8080cb71d9ae5293f8c533a0
Fredy Mauricio Benitez <fredymauricio.benitez.orellana@gmail.com>
2022-03-18 11:56:15 -0600

    Clase 1 curso basico de javascript

---
+++ escuela_javascript/curso_de_javascript_basico/variables.js
@@ -0,0 +1,21 @@
+let nombre = "Fredy";
+let entero = 14;
+let decimal = 0.3;
+let nulo = null;
+let verdadero = true;
+let falso = false;
+let array = [];
+let array2 = (1, 2);
+let array3 = {
+    'nombre': nombre,
+    'entero': entero
+};
+let indefinido = undefined;
+/* COMENTARIO */
+/*
+ESTE
+ES
+UN
+SEGUNDO
+COMENTARIO
+*/
