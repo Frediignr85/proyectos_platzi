704cc3b4b1d4b54308c0f54a03b5d89772f253e4
Fredy Mauricio Benitez <fredymauricio.benitez.orellana@gmail.com>
2022-03-28 15:51:42 -0600

    finalizacion ecmascript11

--- escuela_javascript/curso_ecmascript6/src/es11/index.js
+++ escuela_javascript/curso_ecmascript6/src/es11/index.js
@@ -4,4 +4,28 @@ button.addEventListener("click", async function() {
     const module = await
     import ("./file.js");
     module.hello();
-})
+});
+
+
+//const aBigNumber = 900719925470991 n;
+
+const anotherBigNumber = BigInt(900719925470991);
+console.log(anotherBigNumber);
+
+
+const promise1 = new Promise((resolve, reject) => reject("reject"));
+const promise2 = new Promise((resolve, reject) => resolve("resolve"));
+const promise3 = new Promise((resolve, reject) => resolve("resolve 1"));
+
+Promise.allSettled([promise1, promise2, promise3])
+    .then(response => console.log(response));
+
+
+console.log(window);
+console.log(globalThis);
+
+
+
+
+const fooo = 'hola' ? ? 'default string';
+console.log(fooo);
