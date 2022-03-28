0e022078003d35d73bf125bc1daf464f9e8e0c1c
Fredy Mauricio Benitez <fredymauricio.benitez.orellana@gmail.com>
2022-03-28 14:16:57 -0600

    Forma de imprimir arreglos y saltos de linea

--- escuela_javascript/curso_ecmascript6/src/es6/index.js
+++ escuela_javascript/curso_ecmascript6/src/es6/index.js
@@ -1,3 +1,7 @@
+/* CLASE 1 */
+/* CLASE 1 */
+/* CLASE 1 */
+
 /* FORMA VIEJA DE DECLARAR VALORES POR DEFECTO A LOS PARAMETROS */
 function oldFunction(name, age, country) {
     var name = name || 'Oscar';
@@ -23,3 +27,36 @@ console.log(epicPhrase)
 /* NUEVA FORMA DE CONCATENAR */
 let newPhrase = `${hello} ${world}`;
 console.log(newPhrase);
+
+/* CLASE 2 */
+/* CLASE 2 */
+/* CLASE 2 */
+
+/* Forma vieja de multilinea */
+let lorem = "Esto es una frase nueva \n" +
+    "Esta es la segunda linea de una frase.";
+/* Forma nueva de multilinea */
+let lorem2 = `Esta es una nueva frase para poder cambiar
+la frase la cual nosotros estamos viendo
+todo esto.`;
+console.log(lorem);
+console.log(lorem2);
+
+
+let person = {
+    'name': "Fredy",
+    'age': 24,
+    'country': "El Salvador"
+};
+/* FORMA NORMAL DE RECUPERAR DATOS DE UN ARRAY */
+console.log(person.name, person.age, person.country);
+/* FORMA ALTERNATIVA DE TRAER DATOS DE UN ARREGLO */
+let { name, age, country } = person;
+console.log(name, age, country);
+
+
+let team1 = ['Fredy', 'Tatiana', 'Ricardo'];
+let team2 = ['Vilder', 'Mario', 'Beatriz'];
+
+let education = ['David', ...team1, ...team2];
+console.log(education);
